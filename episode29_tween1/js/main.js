1596bd16d5cc51640776ed96f4ba08811822ce8f
eri1221 <izutsu812@gmail.com>
2021-02-12 19:36:42 +0900

    add: episode29

--- episode29_tween1/js/main.js
+++ episode29_tween1/js/main.js
@@ -4,34 +4,54 @@ const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
-let target = { // 目標到達地点
-  x: width,
-  y: height * Math.random()
-}
-const position = { //現在のposition
-  x: 0,
-  y: height * Math.random()
-}
-const ease = 0.1;
 
-document.body.addEventListener('mousemove', (e) => {
-  target.x = e.clientX;
-  target.y = e.clientY;
-})
+let start = { // 開始位置
+  x: 100,
+  y: 100,
+};
+let target = {};  // 目標値
+let change = {}; // 変化量
+let startTime; //開始時間
+const duration = 1000; //アニメーションする時間
 
-update();
+drawCircle(start.x, start.y);
+
+document.body.addEventListener("click", function (event) {
+  target.x = event.clientX;
+  target.y = event.clientY;
+  change.x = target.x - start.x;
+  change.y = target.y - start.y;
+  startTime = new Date();
+  update();
+});
 
 function update() {
   context.clearRect(0, 0, width, height);
 
+  let time = new Date() - startTime; // 経過時間
+
+  // アニメーションする時間内であれば
+  if (time < duration) {
+    // t: current time, b: beginning value, c: change in value, d: duration
+    const x = easeInOutQuad(time, start.x, change.x, duration);
+    const y = easeInOutQuad(time, start.y, change.y, duration);
+
+    drawCircle(x, y);
+
+    window.requestAnimationFrame(update);
+
+  } else {
+    // アニメーションする時間超えたら目標値と同等にしてアップデートやめる
+    drawCircle(target.x, target.y);
+    start.x = target.x;
+    start.y = target.y;
+  }
+}
+
+function drawCircle(x, y) {
   context.beginPath();
-  context.arc(position.x, position.y, 20, 0, Math.PI * 2, false);
+  context.arc(x, y, 20, 0, Math.PI * 2, false);
   context.fill();
-
-  position.x += (target.x - position.x) * ease;
-  position.y += (target.y - position.y) * ease;
-
-  window.requestAnimationFrame(update);
 }
 
 // --------------------
@@ -72,4 +92,24 @@ class Utils {
       t * t * t * p3.y;
     return pFinal;
   }
+}
+
+///////////// QUADRATIC EASING: t^2 ///////////////////
+
+// quadratic easing in - accelerating from zero velocity
+// t: current time, b: beginning value, c: change in value, d: duration
+// t and d can be in frames or seconds/milliseconds
+function easeInQuad(t, b, c, d) {
+  return c * (t /= d) * t + b;
+}
+
+// quadratic easing out - decelerating to zero velocity
+function easeOutQuad(t, b, c, d) {
+  return -c * (t /= d) * (t - 2) + b;
+}
+
+// quadratic easing in/out - acceleration until halfway, then deceleration
+function easeInOutQuad(t, b, c, d) {
+  if ((t /= d / 2) < 1) return (c / 2) * t * t + b;
+  return (-c / 2) * (--t * (t - 2) - 1) + b;
 }
